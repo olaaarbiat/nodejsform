ebcd282da265fadc43bdd7d1a0190d8ba4950e9d
olaaarbiat <159236938+olaaarbiat@users.noreply.github.com>
2024-02-21 11:27:23 +0300

    Add files via upload

---
+++ nodeform.js
@@ -0,0 +1,29 @@
+const express= require('express')
+
+var app= express();
+
+//var json= require('json')
+
+app.get('/',function(req,res)
+{
+    res.send("start server")
+})
+
+app.get('/form', function(req,res)
+{
+    res.sendFile(__dirname+'/form.html')
+})
+
+var bodyParser= require('body-parser')
+
+var urlEncoded= bodyParser.urlencoded({extended:false})
+
+app.post('/submitform',urlEncoded,function(req,res){
+    res.send("data submitted"+req.body.email+req.body.name)
+})
+
+var server= app.listen(7000, function()
+{
+    var host = server.address().address // local one  
+    var port = server.address().port 
+})
